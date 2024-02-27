2e6d4d26b0d0c3098c8cc9c071fc55c371b713ac
alextheriault4 <alextheriault4@gmail.com>
2024-02-27 12:55:32 -0500

    active game

--- client/src/components/pages/ActiveGameView.js
+++ client/src/components/pages/ActiveGameView.js
@@ -5,6 +5,7 @@ import "./ActiveGameView.css";
 
 const ActiveGameView = (props) => {
   const [activeGames, setActiveGames] = useState([]);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     // Fetch active games data when component mounts
@@ -15,15 +16,34 @@ const ActiveGameView = (props) => {
       .catch((error) => {
         console.error("Error fetching active games:", error);
       });
+
+    get("/api/users")
+      .then((data) => {
+        setUsers(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching users:", error);
+      });
   }, []); // Empty dependency array to run effect only once on mount
 
+  const playerName = (id) => {
+    const user = users.find((user) => user._id === id);
+    return user ? user.name : null;
+  };
+
   return (
-    <div>
+    <div style={{ textAlign: 'center' }}>
       <h1>This is the Active Game Page</h1>
-      <ul>
+      <ul style={{ listStyleType: "none", padding: 0 }}>
         {activeGames.map((game) => (
-          <li key={game._id}>
-            Player 1: {game.player1} {/* Assuming player1 is the name */}
+          <li key={game._id} style={{ position: 'relative', display: 'inline-block' }}>
+            <a href="/">
+              <img src="/table.jpg" style={{ width: '250px' }} />
+            </a>
+            <p style={{ position: 'absolute', top: 60, left: -100 }}>{playerName(game.player1)}</p>
+            <p style={{ position: 'absolute', top: 60, right: -35 }}>{playerName(game.player3)}</p>
+            <p style={{ position: 'absolute', bottom: 50, left: -100 }}>{playerName(game.player2)}</p>
+            <p style={{ position: 'absolute', bottom: 50, right: -35 }}>{playerName(game.player4)}</p>
           </li>
         ))}
       </ul>
